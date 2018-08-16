563fccbc881caede10321ea9b959587284766741
jean.chung <jeannchung@gmail.com>
2018-08-15 20:44:40 -0700

    final working version! i finished!

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -1,10 +1,10 @@
-
 let score = 0
+let oldScore = 0
 let currentWord = ''
 let guessLeft = 10
 let hiddenWord = ''
 let guessedLetters = []
-const allWords = ['Genesis', 'Blondie', ''];
+const allWords = ['Genesis', 'Blondie', 'Journey'];
 let letters = ['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z']
 
 
@@ -67,17 +67,19 @@ function displayValues() {
 
 function lose() {
     guessLeft--
-    displayValues()
     if (guessLeft <= 0) {
         document.querySelector('#message').innerHTML = `<h1>You lost! Press any key to try again.</h1>`
     }
 }
 
-function win() {
+function checkForWin() {
     winWord = hiddenWord.split(' ').join('')
-    if(winWord === currentWord) {
+    console.log(winWord)
+    if(winWord === currentWord.toLowerCase()) {
+        console.log("You should have won!")
         score++
         document.querySelector('#message').innerHTML = `<h1>You won! Press any key to continue.</h1>`
+        document.querySelector('#wins').innerHTML = '' + score
     }
 }
 
@@ -86,6 +88,7 @@ function reset() {
     hiddenWord = setHiddenWord()
     guessLeft = 10
     guessedLetters = []
+    oldScore++
     document.querySelector('#hidden_word').innerHTML = hiddenWord
     document.querySelector('#message').innerHTML = "Press any key to get started!"
     document.querySelector('#guessLeft').innerHTML = '' + guessLeft
@@ -96,7 +99,7 @@ function reset() {
 document.onkeyup = function (event) {
     const letter = event.key
     if (alphaCheck(letter)) {
-        if (guessLeft === 0) {
+        if (guessLeft === 0 || score > oldScore) {
             reset()
             return
         }
@@ -105,25 +108,28 @@ document.onkeyup = function (event) {
             guessedLetters.push(letter);
             console.log(guessedLetters)
             // Can check against hidden word since new letter never seen before
-            for (i = 0; i < currentWord.length; i++) {
-                if (letter === currentWord.charAt(i)) {
-                    x = setCharAt(hiddenWord, (i * 2), letter)
-                    document.querySelector('#hidden_word').innerHTML = x.toString()
-                    console.log(x)
+            let tmpWord = currentWord.toLowerCase();
+            for (i = 0; i < tmpWord.length; i++) {
+                if (letter === tmpWord.charAt(i)) {
+                    hiddenWord = setCharAt(hiddenWord, (i * 2), letter)
+                    document.querySelector('#hidden_word').innerHTML = hiddenWord
+                    console.log(hiddenWord)
                 } 
             }
-            lose()
+
+            checkForWin()
+            //lose()
             // Update UI function
         }
         // If the letter isn't new, why bother checking it? We've checked it before
     }
     console.log(letter)
-    displayValues()
+    //displayValues()
 }
 
 function setCharAt(str, index, chr) {
     if (index > str.length - 1) return str;
-    return str.substr(0, index) + chr + str.substr(index + 1)
+    return str.substr(0, index) + chr + str.substr(index+1)
 }
 
 // CHECKKKKKKKK
@@ -150,3 +156,4 @@ function setCharAt(str, index, chr) {
 // WHAT HAPPENS ON LOSS
 
 // WHAT HAPPENS ON WIN
+
